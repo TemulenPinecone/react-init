b51e0fd032786452a59d44823c8a62c9172292c0
Temulen.G <23lp2607nest.edu.mn@TemulenGs-iMac.local>
2024-01-08 11:45:11 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// JAVASCRIPT FILE ДОТОР HTML TAG БИЧСЭН
+
+import Card from "./components/Card";
+
+export default function App() {
+  return (
+    <div>
+      <p>Hello React</p>
+      <Card />
+      <img src="https://wallpapers.com/images/high/nature-green-background-1920-x-1200-pwwqx25cddnlt13c.webp"></img>
+    </div>
+  );
+}
---
+++ src/components/Card.jsx
@@ -0,0 +1,16 @@
+// export default function Card() {
+//   return (
+//     <div>
+//       <p>Hello Card</p>
+//     </div>
+//   );
+// }
+
+// Arrow function
+const Card = () => {
+  return (
+  <div>
+    <p>Hello Card</p>
+  </div>);
+};
+export default Card
